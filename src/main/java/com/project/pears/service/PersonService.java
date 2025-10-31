d1414ca455d836eccc69e72ecd4fd7f2d5e42523
AnnaGarcia <annaaurea.garcia24@gmail.com>
2025-10-31 14:42:25 -0400

    Implemented Person Service and created Team Repository

--- src/main/java/com/project/pears/service/PersonService.java
+++ src/main/java/com/project/pears/service/PersonService.java
@@ -2,10 +2,13 @@ package com.project.pears.service;
 
 import com.project.pears.dto.PersonDTO;
 import com.project.pears.entity.Person;
+import com.project.pears.entity.Team;
 import com.project.pears.exception.PersonNotFoundException;
 import com.project.pears.factory.PersonFactory;
 import com.project.pears.repository.PersonRepository;
 
+import com.project.pears.repository.TeamRepository;
+import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,7 +19,9 @@ import org.springframework.stereotype.Service;
 public class PersonService {
 
     private final PersonRepository personRepository;
+    private final TeamRepository teamRepository;
     private final PersonFactory factory;
+
 
     public PersonDTO getById(String id) {
         Person person =  personRepository.findById(Long.valueOf(id))
@@ -24,15 +29,39 @@ public class PersonService {
         return factory.toDTO(person);
     }
 
+    @Transactional
     public PersonDTO create(@Valid PersonDTO personDTO) {
-        throw new UnsupportedOperationException("Operation is not yet implemented ");
+        Person entity = factory.toEntity(personDTO);
+
+        entity.setTeam(teamFinder());
+        Person createdEntity = personRepository.save(entity);
+        return factory.toDTO(createdEntity);
     }
 
     public PersonDTO update(String id, @Valid PersonDTO personDTO) {
-        throw new UnsupportedOperationException("Operation is not yet implemented");
+        Person person =  personRepository.findById(Long.valueOf(id))
+                .orElseThrow( () -> new PersonNotFoundException("Person with cannot be found with ID: " + id));
+
+        person.setName(personDTO.getName());
+        person.setRole(personDTO.getRole());
+        Person updatedPerson = personRepository.save(person);
+        return factory.toDTO(updatedPerson);
     }
 
     public void delete(String id) {
-        throw new UnsupportedOperationException("Operation is not yet implemented");
+        Person person =  personRepository.findById(Long.valueOf(id))
+                .orElseThrow( () -> new PersonNotFoundException("Person with cannot be found with ID: " + id));
+        personRepository.delete(person);
+    }
+
+    // Temporary
+    private Team teamFinder() {
+        Team tempTeam = teamRepository.findById(1L)
+                .orElseGet(() -> {
+                    Team t = new Team();
+                    t.setName("TEMP_TEAM");
+                    return teamRepository.save(t);
+                });
+        return tempTeam;
     }
 }
