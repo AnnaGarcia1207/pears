91c1233492b63957c8e42ac13dbd36c4d8fb7c64
AnnaGarcia <annaaurea.garcia24@gmail.com>
2025-11-04 15:44:09 -0500

    wip PairController test

--- src/main/java/com/project/pears/dto/PairDto.java
+++ src/main/java/com/project/pears/dto/PairDto.java
@@ -1,4 +1,13 @@
 package com.project.pears.dto;
 
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Builder
 public class PairDto {
+    private String id;
+    private List<PersonDto> pairs;
 }
--- src/test/java/com/project/pears/controller/PairControllerTest.java
+++ src/test/java/com/project/pears/controller/PairControllerTest.java
@@ -1,6 +1,7 @@
 package com.project.pears.controller;
 
 import com.project.pears.dto.PairDto;
+import com.project.pears.dto.PersonDto;
 import com.project.pears.service.PairService;
 import org.apache.coyote.Response;
 import org.junit.jupiter.api.Test;
@@ -8,9 +9,15 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class PairControllerTest {
@@ -23,9 +30,48 @@ class PairControllerTest {
 
     @Test
     void getById() {
+        String id  = "1";
+        PersonDto pairOne = PersonDto.builder().name("Anna").build();
+        PersonDto pairTwo = PersonDto.builder().name("Andrey").build();
 
-        ResponseEntity<PairDto> response = subject.getById("1L");
-        assertNotNull(response);
+        PairDto expected = PairDto.builder()
+                .id(id)
+                .pairs(List.of(pairOne, pairTwo))
+                .build();
+        // Arrange
+        when(mockPairService.getById(id)).thenReturn(expected);
+        // Act
+        ResponseEntity<PairDto> response = subject.getById(id);
+        // Assert
+        assertNotNull(response.getBody());
+        assertEquals(id, response.getBody().getId());
     }
+
+    @Test
+    void getAllPairs() {
+        String id  = "1";
+        PersonDto pairOne = PersonDto.builder().name("Anna").build();
+        PersonDto pairTwo = PersonDto.builder().name("Andrey").build();
+
+        PairDto expected = PairDto.builder()
+                .id(id)
+                .pairs(List.of(pairOne, pairTwo))
+                .build();
+        // Arrange
+        when(mockPairService.getAllPairs()).thenReturn(List.of(expected));
+        // Act
+        ResponseEntity<List<PairDto>> response = subject.getPairs();
+        // Assert
+        assertNotNull(response.getBody());
+        PairDto actual = response.getBody().getFirst();
+        assertEquals(id, actual.getId());
+
+    }
+
+    @Test
+    void create() {
+
+    }
+
 
 }
